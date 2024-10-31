74e60cdaf816dc97c21d2ed44ed30fbfc79f77c3
Kaustubh Raghavan <you@example.com>
2024-10-31 22:19:39 +0530

    Added ssl

--- src/config/Database.ts
+++ src/config/Database.ts
@@ -7,7 +7,8 @@ export class Database {
 
     constructor() {
         this.pool = new Pool({
-            connectionString : process.env.DATABASE_URL
+            connectionString : process.env.DATABASE_URL,
+            ssl: true
           });
 
 
