fc049ccc4f85c5cb7b0e72cf460fab93e8d184ab
Kaustubh Raghavan <you@example.com>
2024-10-31 23:12:06 +0530

    Using Client Instead of pool

--- src/config/Database.ts
+++ src/config/Database.ts
@@ -1,36 +1,42 @@
 import { injectable } from 'inversify';
-import { Pool, PoolClient } from 'pg';
+import { Client } from 'pg';
 
 @injectable()
 export class Database {
-    private pool: Pool;
+    private client: Client;
 
     constructor() {
-        this.pool = new Pool({
-            connectionString : process.env.DATABASE_URL,
+        this.client = new Client({
+            connectionString: process.env.DATABASE_URL,
             ssl: { rejectUnauthorized: false }
-          });
-
-
-    }
-
-    async query(text: string, params?: any[]) {
-        const client = await this.pool.connect();
-        try {
-            const result = await client.query(text, params);
-            return result.rows;
-        } finally {
-            client.release();
-        }
+        });
     }
 
     async connect(): Promise<void> {
         try {
-            const client: PoolClient = await this.pool.connect();
+            await this.client.connect();
             console.log('Connected to PostgreSQL');
-            client.release();
         } catch (err: any) {
             console.error('Error connecting to PostgreSQL:', err.message);
+        }
+    }
+
+    async query(text: string, params?: any[]) {
+        try {
+            const result = await this.client.query(text, params);
+            return result.rows;
+        } catch (err: any) {
+            console.error('Error executing query:', err.message);
+            throw err; // Optionally rethrow the error
+        }
+    }
+
+    async disconnect(): Promise<void> {
+        try {
+            await this.client.end();
+            console.log('Disconnected from PostgreSQL');
+        } catch (err: any) {
+            console.error('Error disconnecting from PostgreSQL:', err.message);
         }
     }
 }
